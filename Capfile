760b479c11131fdc558bcf7910329ddb5e73da95
Jacob Hodes <jacob@peashutop.com>
2010-02-27 19:28:05 -0500

    move deploy code from Capfile to deploy.rb; deploy:cleanup afterwards

--- Capfile
+++ Capfile
@@ -2,18 +2,3 @@ load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 
 load 'config/deploy' # remove this line to skip loading any of the default tasks
-
-namespace :deploy do
-  task :start, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
-  end
-
-  task :stop, :roles => :app do
-    # Do nothing.
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
-  end
-end
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :port, 37658
 set :branch, "master"
 set :use_sudo,    false
 
+after "deploy", "deploy:cleanup"
 
 role :web, "bc.peashutop.com"                          # Your HTTP server, Apache/etc
 role :app, "bc.peashutop.com"                          # This may be the same as your `Web` server
@@ -15,17 +16,16 @@ role :db,  "bc.peashutop.com", :primary => true # This is where Rails migrations
 
 set :deploy_to, "/home/jacob/#{application}"
 
+set :stages, %w(staging production demo dev jgl)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
 #role :db,  "your slave db-server here"
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
